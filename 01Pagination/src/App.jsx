1b243d21df42d0c5cf045dca3dc10fd35a8fba18
PRAFUL GUPTA <92764978+prafulthe85@users.noreply.github.com>
2026-02-05 19:28:15 +0000

    pagination logic ends here

--- 01Pagination/src/App.jsx
+++ 01Pagination/src/App.jsx
@@ -1,9 +1,84 @@
+import { useEffect, useState } from "react";
+import "./App.css"
 
+const PAGE_SIZE = 12;
+
+const ProductCard = ( {title, image} )=>{
+  return (
+    <div className="s">
+      <span className="product-card-title">{title}</span>
+        <img className="product-card-img" src={image} alt={title} />
+    </div>
+  );
+}
 
 function App() {
 
+  const [products, setProducts] = useState([])
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const fetchData = async () =>{
+    const data = await fetch('https://dummyjson.com/products?limit=200');
+    const jsonData = await data.json();
+    setProducts(jsonData.products);
+  }
+
+  const handlePageChange = (n) =>{
+    setCurrentPage(n);
+  }
+  const handleNextPage = () =>{
+    setCurrentPage((n)=>n+1)
+  }
+  const handlePrevPage = () => {
+    setCurrentPage((n)=>n-1)
+  }
+
+  useEffect(()=>{
+    const loadData = async () =>{
+      await fetchData();
+    }
+    loadData();
+  }, [])
+
+  const totalProducts = products.length;
+  const totalPages = Math.ceil(totalProducts/PAGE_SIZE);
+  const start = currentPage*PAGE_SIZE;
+  const end = start+PAGE_SIZE;
+
+  console.log('products', products);
   return (
-    <>Hello React</>
+    <>
+    <div>
+      <div className="parent-heading">
+        Gmad @React
+      </div>
+      { !products.length ? (
+          <div>No Products found, API is failing</div>
+        ) : (
+          <>
+            <div className="pagination-container">
+              <button className={`page-number ${currentPage===0 ? 'add-class' : ''}`} disabled={currentPage===0} onClick={()=> handlePrevPage()}>Prev</button>
+              {[...Array(totalPages).keys()].map((n)=> (
+                <span className={`page-number ${currentPage===n ? 'active-page' : ''}`} onClick={()=>handlePageChange(n)}>{n+1}</span>
+              ))}
+              <button  className={`page-number ${currentPage===totalPages-1 ? 'add-class' : ''}`} disabled={currentPage===totalPages-1} onClick={()=> handleNextPage()}>Next</button>
+            </div>
+            <div className="product-card-parent">
+              {
+                products.slice(start,end).map((p) => (
+                  <ProductCard 
+                    key={p.id} 
+                    image={p.thumbnail} 
+                    title={p.title} 
+                  />
+                ))
+              }
+            </div>
+          </>
+          )
+        } 
+    </div>
+    </>
   )
 }
 
